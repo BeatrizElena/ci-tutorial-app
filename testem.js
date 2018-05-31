9d92bf429e2e1953864f385bf79ce74a68d8df04
beatrizelena <ortizwebdevelopment@gmail.com>
2018-05-30 21:28:21 -0400

    add firefox support

--- testem.js
+++ testem.js
@@ -2,10 +2,12 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   browser_args: {
     Chrome: {
@@ -20,5 +22,9 @@ module.exports = {
         '--window-size=1440,900'
       ].filter(Boolean)
     }
+    Firefox: [
+      '-headless',
+      '--window-size=1440,900'
+    ]
   }
 };
